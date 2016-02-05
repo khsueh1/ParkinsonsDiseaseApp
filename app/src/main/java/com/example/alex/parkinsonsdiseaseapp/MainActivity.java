4c00cd9c5eab9bb3f2b662b093e95cb9d46dc52b
malexcauthen <mcauthen@vols.utk.edu>
2016-02-05 15:18:40 -0500

    Basic User Interface
    Added some buttons and a second activity that displays a generic test
    scene.

--- app/src/main/java/com/example/alex/parkinsonsdiseaseapp/MainActivity.java
+++ app/src/main/java/com/example/alex/parkinsonsdiseaseapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alex.parkinsonsdiseaseapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button startTestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        startTestButton = (Button) findViewById(R.id.testButton);
+        startTestButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -48,5 +45,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
     }
 }

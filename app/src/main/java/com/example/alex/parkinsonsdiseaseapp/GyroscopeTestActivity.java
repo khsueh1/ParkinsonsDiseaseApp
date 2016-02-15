8a637aeb9b3cc33d0afb06d78f78bc76a0f170d5
cbower3 <cbower3@vols.utk.edu>
2016-02-14 20:01:46 -0500

    test

--- app/src/main/java/com/example/alex/parkinsonsdiseaseapp/GyroscopeTestActivity.java
+++ app/src/main/java/com/example/alex/parkinsonsdiseaseapp/GyroscopeTestActivity.java
@@ -1,13 +1,43 @@
 package com.example.alex.parkinsonsdiseaseapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
-public class GyroscopeTestActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+public class GyroscopeTestActivity extends AppCompatActivity implements SensorEventListener {
+    private  SensorManager sm;
+    private  Sensor mAcc;
+
+    protected void onResume(){
+        super.onResume();
+        sm.registerListener(this, mAcc, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause(){
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +45,21 @@ public class GyroscopeTestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gryoscope_test);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAcc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public void onSensorChanged(SensorEvent e) {
+        String output = "x = " + e.values[0] + ", Y = " + e.values[1] + "; Z = " + e.values[2];
+       /* try {
+            FileOutputStream outputStream = openFileOutput("test.txt", Context.MODE_WORLD_READABLE);
+            outputStream.write(output.getBytes());
+            outputStream.close();
+        } catch (Exception z) {
+            z.printStackTrace();
+        }*/
+        System.out.println(output);
     }
 
 }

48da45cff26222e2a0d590bb63f635ce5fce8ec6
malexcauthen <mcauthen@vols.utk.edu>
2016-02-17 14:12:07 -0500

    Email
    Can send an email by pressing email button

--- app/src/main/java/com/example/alex/parkinsonsdiseaseapp/GyroscopeTestActivity.java
+++ app/src/main/java/com/example/alex/parkinsonsdiseaseapp/GyroscopeTestActivity.java
@@ -1,17 +1,22 @@
 package com.example.alex.parkinsonsdiseaseapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -48,6 +53,13 @@ public class GyroscopeTestActivity extends AppCompatActivity implements SensorEv
 
         sm = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAcc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        Button email = (Button) findViewById(R.id.emailButton);
+        email.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
     }
 
     public void onSensorChanged(SensorEvent e) {
@@ -62,4 +74,26 @@ public class GyroscopeTestActivity extends AppCompatActivity implements SensorEv
         System.out.println(output);
     }
 
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {""};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "test");
+        //emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(GyroscopeTestActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }

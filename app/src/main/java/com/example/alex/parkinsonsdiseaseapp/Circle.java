83877c56bb86c503727c0d8b96c2b725defd51f8
khsueh1 <kenneth.hsueh@gmail.com>
2016-04-16 23:46:51 -0400

    KENNY IS A GOD

--- app/src/main/java/com/example/alex/parkinsonsdiseaseapp/Circle.java
+++ app/src/main/java/com/example/alex/parkinsonsdiseaseapp/Circle.java
@@ -22,12 +22,11 @@ public class Circle extends View {
     private Random random = new Random();
     public static ArrayList<Double> distances;
     int flag = 1;
-    static int startFlag = 0;
     Data cData;
 
     public class Data {
         int count;
-        ArrayList <Double> singleDist;
+        ArrayList<Double> singleDist;
 
         public Data() {
             this.count = 0;
@@ -38,12 +37,10 @@ public class Circle extends View {
     // draws circle
     @Override
     protected void onDraw(Canvas canvas) {
-        if (startFlag == 1) {
-            super.onDraw(canvas);
-            mPaint.setStyle(Paint.Style.FILL);
-            mPaint.setColor(Color.RED);
-            canvas.drawCircle(x, y, r, mPaint);
-        }
+        super.onDraw(canvas);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.RED);
+        canvas.drawCircle(x, y, r, mPaint);
     }
 
     // constructors
@@ -75,10 +72,9 @@ public class Circle extends View {
 
         int border = r + 50;
 
-        this.x = border + random.nextInt(w-border);
-        this.y = border + random.nextInt(h-border);
+        this.x = border + random.nextInt(w - border);
+        this.y = border + random.nextInt(h - border);
     }
-
 
 
     // when screen is tapped, old circle removed, new circle drawn
@@ -87,7 +83,7 @@ public class Circle extends View {
         if (flag == 1) {
             flag = 0;
             distances = new ArrayList<Double>();
-           // cData = new Data();
+            // cData = new Data();
 
         }
 
@@ -107,6 +103,8 @@ public class Circle extends View {
 
     void distance(double newX, double newY) {
         distances.add(Math.sqrt(Math.pow(x - newX, 2) + Math.pow(y - newY, 2)));
+
+        System.out.println(Math.sqrt(Math.pow(x - newX, 2) + Math.pow(y - newY, 2)));
     }
 
     public boolean isInsideCircle(float xPoint, float yPoint) {
